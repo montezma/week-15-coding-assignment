8312d68fa55161a2dca5f547f70bc53ca254713a
montezma <montez.noah@yahoo.com>
2022-11-11 12:13:50 -0800

    updated

--- src/rest/HousesAPI.js
+++ src/rest/HousesAPI.js
@@ -3,15 +3,10 @@ const HOUSES_ENDPOINT = "https://ancient-taiga-31359.herokuapp.com/api/houses";
 
 class HousesApi {
     get = async () => {
-        //try is to see if it working cause it's an network request.
         try {
-            //makes request
             const resp = await fetch(HOUSES_ENDPOINT); 
-            // waits from get request then converts to json
             const data = await resp.json();
-            //returns the data 
             return data; 
-            //catch it to see if it errors out it logs the error.
         } catch(e) {
             console.log("get request didnt work",e)
         }
